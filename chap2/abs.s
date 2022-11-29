321131d4b4ca4b41f693ad5b039acc676d05c267
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2022-11-29 05:14:21 +0000

    reduce commnand

--- chap2/abs.s
+++ chap2/abs.s
@@ -5,11 +5,11 @@
 	.global _start
 _start:
 	mov r1, #N
-	mov r2, r1
-	eor r3, r1, r2, asr #31
-	sub r0, r3, r2, asr #31
+	eor r3, r1, r1, asr #31
+	sub r0, r3, r1, asr #31
 	
 
+  @; EXIT system call
 	mov r7, #1
 	swi #0
  
