b0dbfad9c23146e9724040f34448ef3eb4d8ef7a
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2022-12-02 04:41:20 +0000

    remove mnemonic

--- chap3/fib13.s
+++ chap3/fib13.s
@@ -3,14 +3,14 @@
 
 _start:
   mov r1, #1  @; n-1
-  mov r3, #13 @; n
+  mov r3, #12 @; n
 calc:
-  subs  r3, #1
-  beq endp
+  subs  r3, r3, #1
   add r0, r1, r2
   mov r2, r1
   mov r1, r0
-  b calc
-endp:   @; EXIT Processing
+  bne calc
+  @; EXET system call
+endp:
   mov r7, #1
   swi #0
