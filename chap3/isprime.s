2d85387fb4404744aa48e3c8b189149c88b7ee7d
MIKAMI shu <250372y@ugs.kochi-tech.ac.jp>
2022-12-02 17:15:12 +0900

    remove 0,1

--- chap3/isprime.s
+++ chap3/isprime.s
@@ -1,4 +1,4 @@
-	.equ N, 18
+	.equ N, 0
 	
 	.section .text
 	.global _start
@@ -6,19 +6,20 @@
 	
 _start:
 	ldr r1, =N
-	@mov r2, #1   なくても良い
-	ldr r2, =N
+	mov r2, #1
 	mov r0, #1
+	cmp r1, #2
+	bcc end
 
-@loop0: 高速化を狙った処理
-	@mul r3, r2, r2
-	@cmp r1, r3
-	@add r2, r2, #1
-	@bpl loop0
+loop0: 
+	mul r3, r2, r2
+	cmp r1, r3
+	add r2, r2, #1
+	bpl loop0
 	
 loop1:
 	subs r2, r2, #1
-	cmp r2, #1
+	cmp r2, #2
 	moveq r0, #0
 	beq end
 	udiv r4, r1, r2
