a721554b1efbac2febd31dbda1921155fd859ab0
MIKAMI shu <250372y@ugs.kochi-tech.ac.jp>
2022-12-02 16:28:37 +0900

    code golf

--- chap3/isprime.s
+++ chap3/isprime.s
@@ -1,4 +1,4 @@
-	.equ N, 41
+	.equ N, 20000
 	
 	.section .text
 	.global _start
@@ -7,32 +7,24 @@
 _start:
 	ldr r1, =N
 	mov r2, #1
+	mov r0, #1
 
 loop0:
 	mul r3, r2, r2
 	cmp r1, r3
-	bls loop1
 	add r2, r2, #1
-	b loop0
+	bpl loop0
 
 loop1:
+	subs r2, r2, #1
 	cmp r2, #1
-	beq prime
+	moveq r0, #0
+	beq end
 	udiv r4, r1, r2
 	mul r5, r2, r4
 	subs r6, r1, r5
-	beq notprime
-	sub r2, r2, #1
-	b loop1
-
-prime:	mov r8, #0
-	b end
-
-notprime:
-	mov r8, #1
-	b end
+	bne loop1
 
 end:
 	mov r7, #1
-	mov r0, r8
 	swi #0
