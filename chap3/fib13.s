de32cac6a3118b38eb9b8eb59e68537a5a83356f
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2022-12-02 03:50:51 +0000

    executed successfully

---
+++ chap3/fib13.s
@@ -0,0 +1,19 @@
+  .section  .text
+  .global   _start
+
+_start:
+  mov r1, #1  @; n-1
+  mov r2, #0  @; n-2
+  mov r3, #5  @; n
+calc:
+  subs  r3, #1
+  cmp r3, #0
+  beq endp
+  add r0, r1, r2
+  mov r2, r1
+  mov r1, r0
+
+  b calc
+endp:   @; EXIT Processing
+  mov r7, #1
+  swi #0
