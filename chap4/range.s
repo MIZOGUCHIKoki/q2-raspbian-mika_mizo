36955eda1598f13143a30ece0efda15af11de8f2
MIKAMI shu <250372y@ugs.kochi-tech.ac.jp>
2022-12-06 12:22:55 +0900

    r2を読み込むのをldrsbに変更

--- chap4/range.s
+++ chap4/range.s
@@ -2,9 +2,9 @@
 	.global _start
 
 _start:
-	ldrsb r1, =N
+	ldr r1, =N
 	ldr r2, =ndata
-	ldr r9, [r2]
+	ldrsb r9, [r2]
 	ldr r3, =data1
 
 loop0:
@@ -27,6 +27,6 @@ end:
 
 
 	.section .data
-data1:	.word 0, -1, -123, 54, 39, 32, 40
+data1:	.word 0, -1, -123, 54, 39, 32, -39
 ndata:	.word 7
 .equ    N, 40
