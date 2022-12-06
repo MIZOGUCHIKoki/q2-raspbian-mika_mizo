c571f251ffa3ea1f7a9556b5e55432e0a56991d5
MIKAMI shu <250372y@ugs.kochi-tech.ac.jp>
2022-12-06 15:03:26 +0900

    一つ前に戻した

--- chap4/range.s
+++ chap4/range.s
@@ -7,12 +7,13 @@ _start:
 	ldr r3, =data1
 
 loop0:
-	ldr r4, [r3, r2, lsl #2]
+	ldr r4, [r3]
 	eor r5, r4, r4, asr #31
 	sub r6, r5, r4, asr #31  @r6 = abs
 	cmp r1, r6
 	addcs r0, r0, #1
 	subs r2, r2, #1
+	add r3, r3, #4
 	bne loop0
 
 	mov r7, #1
