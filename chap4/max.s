e6c2fbd1b3616c3a7d8b97e30bf06b1d8f87fa1f
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2022-12-06 13:56:11 +0600

    executed successfully

--- chap4/max.s
+++ chap4/max.s
@@ -1,17 +1,17 @@
-	.section  .text
-	.global   _start
+  .section  .text
+  .global   _start
 
 _start:
-	ldr r1, =data1
-	ldr r2, =ndata
+  ldr r1, =data1
+  ldr r2, =ndata
 loop:
 	ldr   r3,   [r1], #4
 	cmp   r3,   r0
-	movhi r0, r3
-	subs r2, r2, #1
+	movhi r0,   r3
+	subs  r2,   r2,   #1
 	bne loop
 	
-	mov r7, #1
+	mov r7,      #1
 	swi #0
 
 	.section  .data
