f1b17ca04dde10a92b873a50930b8e43a27908da
MIKAMI shu <250372y@ugs.kochi-tech.ac.jp>
2022-12-06 14:27:02 +0900

    addpl changed addcs

--- chap4/range.s
+++ chap4/range.s
@@ -6,14 +6,13 @@ _start:
 	ldr r2, =ndata
 	ldr r9, [r2]
 	ldr r3, =data1
-	mov r8, #0
 
 loop0:
 	ldr r4, [r3]
 	eor r5, r4, r4, asr #31
 	sub r6, r5, r4, asr #31  @r6 = abs
 	cmp r1, r6
-	addpl r8, r8, #1
+	addcs r8, r8, #1
 	subs r9, r9, #1
 	beq end
 	add r3, r3, #4
