d5c4b074a82549d1639ab84ba51f9e097a89a399
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2022-12-06 12:34:43 +0600

    accept code golf

--- chap4/printdec.s
+++ chap4/printdec.s
@@ -1,4 +1,4 @@
-  .equ  N,  0
+  .equ  N,  3128
   .equ  ndigit, 100
   .section  .text
   .global   _start
@@ -6,39 +6,39 @@
 _start:
   ldr r10,=N  @; 入力数値
   mov r0, #10 @; 割る数
-  mov r8, #1  @; 桁数
+  mov r2, #1  @; 桁数
 
 countK: @; 桁数Counter
-  mov   r2, #0
+  mov   r4, #0
   udiv  r3, r10,  r0    @;  1932 / 10 = 193
   cmp   r3, #0
-  addne r8, r8, #1      @;  桁数++
+  addne r2, r2, #1      @;  桁数++
   movne r10,r3
   bne   countK
-@; r8  :: 桁数
+@; r2  :: 桁数
 @; r10 :: 入力数値
 writeProcess:
   ldr   r10,  =N
-  ldr   r2,   =buf+ndigit  @; r2 = buf + ndigit 末尾番地
-  sub   r2, r2, #1         @; Return Code space
+  ldr   r1,   =buf+ndigit-1  @; r1 = buf + ndigit 末尾番地
+  @;sub   r1, r1, #1         @; Return Code space
 wloop:
   udiv  r4, r10,  r0  @; N / 10 = r4
   @; A divid by B = R mod Q <=> A - BR = Q
   mul   r5, r0,   r4  @; BR
   sub   r5, r10,  r5  @; r5 = N % 10
   add   r5, r5,   #'0'
-  strb  r5, [r2], #-1      @;  [r2] <- r5 (0拡張); r2--
+  strb  r5, [r1], #-1      @;  [r1] <- r5 (0拡張); r2--
   mov   r10,r4
   cmp   r4, #0
   bne   wloop
   
 write:
-  add r8, r8, #1  @;  Return Code
+  add r1, r1, #1
   add r2, r2, #1  @;  先頭番地++
   mov r7, #4
   mov r0, #1
-  mov r1, r2      @;  先頭番地
-  mov r2, r8      @;  長さ
+  @;mov r1, r2      @;  先頭番地
+  @;mov r2, r2      @;  長さ
   swi #0
 endp:
   mov r7, #1
