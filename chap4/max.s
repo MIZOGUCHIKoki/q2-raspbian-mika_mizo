65d81b7288af16327d84530e7139d44cf0e74888
MIKAMI shu <250372y@ugs.kochi-tech.ac.jp>
2022-12-06 16:03:48 +0900

    codegolf

--- chap4/max.s
+++ chap4/max.s
@@ -1,23 +1,19 @@
-  .section  .text
-  .global   _start
+	.section  .text
+	.global   _start
 
 _start:
-  ldr r1, =data1
-  ldr r2, =ndata
+	ldr r1, =data1
+	ldr r2, =ndata
 loop:
-  subs  r2, r2, #1
-  bmi   endp
-  ldr   r3,   [r1], #4
-  cmp   r3,   r0
-  bls   lsp
-  ins:
-    mov r0, r3
-  lsp:
-    b loop
-endp:
-  mov r7, #1
-  swi #0
+	ldr   r3,   [r1], #4
+	cmp   r3,   r0
+	movhi r0, r3
+	subs r2, r2, #1
+	bne loop
+	
+	mov r7, #1
+	swi #0
 
-  .section  .data
+	.section  .data
 data1:  .word 10,2,1,20
-.equ  ndata,  4
+	.equ  ndata,  4
