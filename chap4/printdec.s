16f18738f453db5be943ed0b64c0ef6ae64b3152
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2022-12-06 11:30:41 +0600

    executed successfully

--- chap4/printdec.s
+++ chap4/printdec.s
@@ -1,4 +1,4 @@
-  .equ  N,  1932
+  .equ  N,  0
   .equ  ndigit, 100
   .section  .text
   .global   _start
@@ -20,22 +20,23 @@ countK: @; 桁数Counter
 writeProcess:
   ldr   r10,  =N
   ldr   r2,   =buf+ndigit  @; r2 = buf + ndigit 末尾番地
-  sub   r2, r2, #1
+  sub   r2, r2, #1         @; Return Code space
 wloop:
   udiv  r4, r10,  r0  @; N / 10 = r4
-  cmp   r4, #0
-  beq   write
   @; A divid by B = R mod Q <=> A - BR = Q
   mul   r5, r0,   r4  @; BR
   sub   r5, r10,  r5  @; r5 = N % 10
   add   r5, r5,   #'0'
-  strb  r5, [r2, #-1]!      @;  [r2] <- r5 (0拡張); r2--
-  b     wloop
-
+  strb  r5, [r2], #-1      @;  [r2] <- r5 (0拡張); r2--
+  mov   r10,r4
+  cmp   r4, #0
+  bne   wloop
+  
 write:
   add r8, r8, #1  @;  Return Code
+  add r2, r2, #1  @;  先頭番地++
   mov r7, #4
-  mov r0, #0
+  mov r0, #1
   mov r1, r2      @;  先頭番地
   mov r2, r8      @;  長さ
   swi #0
@@ -46,4 +47,4 @@ endp:
 
   .section  .data
 buf:  .space ndigit  @;  100Byte
-      .word  0x0a    @;  Return Code
+      .ascii "\n"    @;  Return Code
