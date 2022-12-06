5ba2b30a4db52514a46c77cd8fab4e3ba64d1aed
MIKAMI shu <250372y@ugs.kochi-tech.ac.jp>
2022-12-06 15:24:46 +0900

    codegolf set ndata-1

--- chap4/range.s
+++ chap4/range.s
@@ -3,18 +3,17 @@
 
 _start:
 	ldr r1, =N
-	ldr r2, =ndata
+	ldr r2, =ndata-1
 	ldr r3, =data1
 
 loop0:
-	ldr r4, [r3]
+	ldr r4, [r3, r2, lsl #2]
 	eor r5, r4, r4, asr #31
 	sub r6, r5, r4, asr #31  @r6 = abs
 	cmp r1, r6
 	addcs r0, r0, #1
 	subs r2, r2, #1
-	add r3, r3, #4
-	bne loop0
+	bpl loop0
 
 	mov r7, #1
 	swi #0
