54711c3e5cf0e84bfa6183b5907abba882229060
MIKAMI shu <250372y@ugs.kochi-tech.ac.jp>
2022-12-09 10:10:12 +0900

    push and pop

--- chap5/grouping.s
+++ chap5/grouping.s
@@ -1,5 +1,4 @@
 	.section .text
-	.g	.section .text
 	.global grouping
 
 grouping:
@@ -17,21 +16,20 @@ grouping:
 	str r2, [sp, #-4]!
 	str r1, [sp, #-4]!
 
-start:lobal grouping
 
-grouping:
-	str r13, [sp, #-4]!
-	str r12, [sp, #-4]!
-	str r11, [sp, #-4]!
-	str r10, [sp, #-4]!
-	str r9, [sp, #-4]!
-	str r8, [sp, #-4]!
-	str r7, [sp, #-4]!
-	str r6, [sp, #-4]!
-	str r5, [sp, #-4]!
-	str r4, [sp, #-4]!
-	str r3, [sp, #-4]!
-	str r2, [sp, #-4]!
-	str r1, [sp, #-4]!
-
-start:
+end:
+	
+	ldr r1, [sp], #4
+	ldr r2, [sp], #4
+	ldr r3, [sp], #4
+	ldr r4, [sp], #4
+	ldr r5, [sp], #4
+	ldr r6, [sp], #4
+	ldr r7, [sp], #4
+	ldr r8, [sp], #4
+	ldr r9, [sp], #4
+	ldr r10, [sp], #4
+	ldr r11, [sp], #4
+	ldr r12, [sp], #4
+	ldr r13, [sp], #4
+	
