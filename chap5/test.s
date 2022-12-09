fa34ea604455bd239348015ffef50e32d4c0330e
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2022-12-09 08:29:07 +0600

    added register operation mnemonic

--- chap5/test.s
+++ chap5/test.s
@@ -2,6 +2,21 @@
   .global   _start
 
 _start:
+  mov r0, #100
+  mov r1, #100
+  mov r2, #100
+  mov r3, #100
+  mov r4, #100
+  mov r5, #100
+  mov r6, #100
+  mov r7, #100
+  mov r8, #100
+  mov r9, #100
+  mov r10, #100
+  mov r11, #100
+  mov r12, #100
+  mov r14, #100
+
   ldr r0, =4294967295
   bl  print_r0
   bl  print_r0
