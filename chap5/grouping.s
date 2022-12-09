2217e0d6c4df907a2ad807528effc6a07df0fbfa
MIKAMI shu <250372y@ugs.kochi-tech.ac.jp>
2022-12-09 10:00:24 +0900

    only push

---
+++ chap5/grouping.s
@@ -0,0 +1,37 @@
+	.section .text
+	.g	.section .text
+	.global grouping
+
+grouping:
+	str r13, [sp, #-4]!
+	str r12, [sp, #-4]!
+	str r11, [sp, #-4]!
+	str r10, [sp, #-4]!
+	str r9, [sp, #-4]!
+	str r8, [sp, #-4]!
+	str r7, [sp, #-4]!
+	str r6, [sp, #-4]!
+	str r5, [sp, #-4]!
+	str r4, [sp, #-4]!
+	str r3, [sp, #-4]!
+	str r2, [sp, #-4]!
+	str r1, [sp, #-4]!
+
+start:lobal grouping
+
+grouping:
+	str r13, [sp, #-4]!
+	str r12, [sp, #-4]!
+	str r11, [sp, #-4]!
+	str r10, [sp, #-4]!
+	str r9, [sp, #-4]!
+	str r8, [sp, #-4]!
+	str r7, [sp, #-4]!
+	str r6, [sp, #-4]!
+	str r5, [sp, #-4]!
+	str r4, [sp, #-4]!
+	str r3, [sp, #-4]!
+	str r2, [sp, #-4]!
+	str r1, [sp, #-4]!
+
+start:
